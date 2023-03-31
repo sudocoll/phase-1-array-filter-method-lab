cc5799d3e20659105fe3aa060872085ae0716c24
Collette Charles <collettecra123@gmail.com>
2023-03-31 15:10:12 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+console.log(findMatching())
+
+function fuzzyMatch(drivers,name){
+    return drivers.filter(driver => driver[0] === name[0])
+}
+
+function matchName(drivers,name){
+  
+    const matches= drivers.filter(driver => driver.name === name);
+
+    return matches
+}
